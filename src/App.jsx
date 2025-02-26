f9ed89c11f057b9014a0af1d5c73893be93ec46f
Naziya-3101 <naaz.sheikh46@gmail.com>
2025-02-26 14:26:18 +0530

    Notes Updated

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Notes from './Component/Notes'
+
+
+function App() {
+  
+
+  return (
+    
+      <div>
+       
+       <Notes/>
+       
+      </div>
+
+  )
+}
+
+export default App
