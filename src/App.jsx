07586af91e579e1efde9df6e0949a00eee8cd60a
Naziya-3101 <naaz.sheikh46@gmail.com>
2025-02-28 09:46:39 +0530

    Note Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import './App.css'
-import Notes from './Component/Notes'
+import { useEffect, useState } from 'react';
+import Navbar from './Component/Navbar';
+import Notes from './Component/Notes';
 
 
 function App() {
-  
+  const [dark, setDark] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  // dark mode function
+  const toggleDarkMode = () => {
+    setDark(prevDark => !prevDark);
+    document.body.style.backgroundColor = dark ? "white" : "black";
+    document.body.style.color = dark ? "black" : "green";
+  };
 
   return (
-    
-      <div>
-       
-       <Notes/>
-       
-      </div>
-
-  )
+    <div className={dark ? "dark-mode" : "light-mode"}>
+      <Navbar dark={dark} toggleDarkMode={toggleDarkMode} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      
+      
+      <Notes searchTerm={searchTerm}/>
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
